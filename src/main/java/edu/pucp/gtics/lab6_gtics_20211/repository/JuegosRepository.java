c1185d4c0a438cd4bb88ef66d66eabaa9456f882
Diego García <a20172367@pucp.edu.pe>
2021-05-17 19:39:59 -0500

    2.1

--- src/main/java/edu/pucp/gtics/lab6_gtics_20211/repository/JuegosRepository.java
+++ src/main/java/edu/pucp/gtics/lab6_gtics_20211/repository/JuegosRepository.java
@@ -10,6 +10,6 @@ import java.util.List;
 @Repository
 
 public interface JuegosRepository extends JpaRepository<Juegos,Integer> {
-    @Query(value = "Select ... Where u.idusuario= ?",nativeQuery = true)
+    @Query(value = "select j.nombre as nombre, j.descripcion as descripcion, j.image as imageurl, j.idgenero as genero  from juegos j, usuarios u, juegosxusuario ju where j.idjuego=ju.idjuego and u.idusuario=ju.idusuario and u.idusuario=?1",nativeQuery = true)
     List<JuegosUserDto> obtenerJuegosPorUser(int idusuario);
 }
